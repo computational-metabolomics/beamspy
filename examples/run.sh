ec2fbf0facfb0cf7349a4921aac6e26680355a71
RJMW <r.j.weber@bham.ac.uk>
2021-01-20 22:06:47 +0000

    Update examples

--- examples/run.sh
+++ examples/run.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 beamspy group-features \
---peaklist ../tests/test_data/variableMetadata.txt \
---intensity-matrix ../tests/test_data/dataMatrix.txt \
---gml ../tests/test_results/graph.gml \
---db ../tests/test_results/results.sqlite \
+--peaklist ../tests/test_data/peaklist_lcms_pos_theoretical.txt \
+--intensity-matrix ../tests/test_data/dataMatrix_lcms_theoretical.txt \
+--gml graph.gml \
+--db results.sqlite \
 --max-rt-diff 5.0 \
 --method pearson \
 --coeff-threshold 0.7 \
 --pvalue-threshold 0.01
 
 beamspy annotate-peak-patterns \
---peaklist ../tests/test_data/variableMetadata.txt \
---intensity-matrix ../tests/test_data/dataMatrix.txt \
---gml ../tests/test_results/graph.gml \
---db ../tests/test_results/results.sqlite \
+--peaklist ../tests/test_data/peaklist_lcms_pos_theoretical.txt \
+--intensity-matrix ../tests/test_data/dataMatrix_lcms_theoretical.txt \
+--gml graph.gml \
+--db results.sqlite \
 --adducts \
 --adducts-library ../beamspy/data/adducts.txt \
 --isotopes \
@@ -22,26 +22,18 @@ beamspy annotate-peak-patterns \
 --ion-mode pos \
 --ppm 5.0
 
-beamspy annotate-mf \
---peaklist ../tests/test_data/variableMetadata.txt \
---intensity-matrix ../tests/test_data/dataMatrix.txt \
---db ../tests/test_results/results.sqlite \
---adducts-library ../beamspy/data/adducts.txt \
---ion-mode pos \
---ppm 3.0
-
 beamspy annotate-compounds \
---peaklist ../tests/test_data/variableMetadata.txt \
---intensity-matrix ../tests/test_data/dataMatrix.txt \
---db ../tests/test_results/results.sqlite \
---db-name hmdb_full_v4_0_v1 \
+--peaklist ../tests/test_data/peaklist_lcms_pos_theoretical.txt \
+--intensity-matrix ../tests/test_data/dataMatrix_lcms_theoretical.txt \
+--db results.sqlite \
+--db-name hmdb_full_v4_0_20200909_v1 \
 --adducts-library ../beamspy/data/adducts.txt \
 --ion-mode pos \
 --ppm 3.0
 
 beamspy summary-results \
---peaklist ../tests/test_data/variableMetadata.txt \
---intensity-matrix ../tests/test_data/dataMatrix.txt \
---db ../tests/test_results/results.sqlite \
---output ../tests/test_results/summary.txt \
+--peaklist ../tests/test_data/peaklist_lcms_pos_theoretical.txt \
+--intensity-matrix ../tests/test_data/dataMatrix_lcms_theoretical.txt \
+--db results.sqlite \
+--output summary.txt \
 --sep tab
